1225d92d824332c78836d1f2615e13a521c08f6f
Anchen <acmhuang@gmail.com>
2019-08-10 13:13:01 -0400

    implemented axios api request for days in application js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,60 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+import Appointment from "components/Appointment";
+import axios from "axios";
 
-export default function Application() {
-  const [day, dayhandler] = useState("Monday");
-
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+    interview: {
+      student: "JayJay",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
     }
-  ];
+  },
+  {
+    id: 2,
+    time: "2pm",
+    interview: {
+      student: "Tyler",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "V",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "4pm"
+  }
+];
+
+export default function Application(props) {
+  const [day, setDate] = useState("Monday");
+  const [days, setDays] = useState([]);
+  // const [interviewer, setInterviewer] = useState(1);
+
+  axios.get("http://localhost:3001/api/days").then(response => {
+    setDays(response.data);
+  });
 
   return (
     <main className="layout">
@@ -31,19 +64,25 @@ export default function Application() {
           src="images/logo.png"
           alt="Interview Scheduler"
         />
+
         <hr className="sidebar__separator sidebar--centered" />
-        <DayList days={days} day={day} setDay={dayhandler} />
+        <DayList days={days} day={day} setDay={setDate} />
         <nav className="sidebar__menu" />
+
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
-        {/* Replace this with the sidebar elements during the "Environment Setup" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id}
+            time={appointment.time}
+            interview={appointment.interview}
+          />
+        ))}
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,6 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Confirm from "components/Appointment/Confirm";
-import Status from "components/Appointment/Status";
-import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form";
 
 import "components/Appointment/styles.scss";
 
@@ -14,52 +10,16 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      <Empty
-        onAdd={() => {
-          console.log("Hello its empty!");
-        }}
-      />
-      <Show
-        student={"Lydia Miller-Jones"}
-        interviewer={{
-          id: 1,
-          name: "Sylvia Palmer",
-          avatar: "https://i.imgur.com/LpaY82x.png"
-        }}
-        onEdit={() => {
-          console.log("Hello its editing for you!");
-        }}
-        onDelete={() => {
-          console.log("Hello its deleting for you!");
-        }}
-      />
-      <Confirm
-        message={"Delete the appointment?"}
-        onConfirm={() => {
-          console.log("Hello do you want to confirm deleting?");
-        }}
-        onCancel={() => {
-          console.log("Hello do you want to cancel deleting?");
-        }}
-      />
-      <Status message={"Deleting"} />
-      <Error
-        errorMessage={"Error"}
-        message={"Could not delete appointment."}
-        onClose={() => {
-          console.log("this is a X symbol!");
-        }}
-      />
-      <Form
-        onCancel={() => {
-          console.log("item cancelled");
-        }}
-        onSave={() => {
-          console.log("item cancelled");
-        }}
-      />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        />
+      ) : (
+        <Empty onAdd={props.onAdd} />
+      )}
     </article>
-    // {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={()=>{console.log("Edit clicked")}} onDelete={()=>{console.log("Delete clicked")}} /> : null }
-    // </article>;
   );
 }
